be712139bd73db1a57aa145c440ee02c45c11db0
Abderrahman Youabd <youabd50@gmail.com>
2025-02-02 16:05:07 +0100

    initial setup

---
+++ src/test/java/com/a1st/grapqlspringboot/GrapqlSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a1st.grapqlspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrapqlSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
