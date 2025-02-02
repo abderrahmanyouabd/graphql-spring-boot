fcb5eea0f7a6401d7c08f0a4dc0a6043d16bea6c
Abderrahman Youabd <youabd50@gmail.com>
2025-02-02 16:27:18 +0100

    feat: add GraphQlRepository annotation to BookRepository

--- src/main/java/com/a1st/grapqlspringboot/book/BookRepository.java
+++ src/main/java/com/a1st/grapqlspringboot/book/BookRepository.java
@@ -2,11 +2,13 @@ package com.a1st.grapqlspringboot.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
+import org.springframework.graphql.data.GraphQlRepository;
 
 /**
  * @author: Abderrahman Youabd aka: A1ST
  * @version: 1.0
  */
+@GraphQlRepository
 public interface BookRepository extends JpaRepository<Book, Long>, QueryByExampleExecutor<Book> {
 
 }
